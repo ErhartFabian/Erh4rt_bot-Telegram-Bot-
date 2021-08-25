bb8e18df4123ea56809bdc93f58038de4939bd20
ErhartFabian <A01274089@itesm.mx>
2021-08-24 23:51:32 -0500

    Basic Message

--- Erh4rt_bot.py
+++ Erh4rt_bot.py
@@ -2,4 +2,10 @@ import os
 import telebot
 
 API_KEY = os.getenv('API_KEY')
+bot = telebot.Telebot(API_KEY)
 
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+    bot.reply_to(message,"Hola!, como va todo?")
+
+bot.polling()
