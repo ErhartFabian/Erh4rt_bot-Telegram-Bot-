588448adbc77b65312a9ad933402749cee6bab64
ErhartFabian <A01274089@itesm.mx>
2021-08-25 00:35:24 -0500

    Star,Greet and Defaul message functions

--- Erh4rt_bot.py
+++ Erh4rt_bot.py
@@ -1,11 +1,19 @@
 import os
 import telebot
 
-API_KEY = os.getenv('API_KEY')
-bot = telebot.TeleBot("API_KEY", parse_mode=None)
+#API_KEY = os.getenv('API_KEY') // ARREGLAR
+bot = telebot.TeleBot('1942968058:AAE9dqYzQqnUw_ctwZUjRDS8YeQTOL3zHh4')
 
-@bot.message_handler(commands=['Greet'])
+#Mensaje de bienvenida comandos Start y Greet
+@bot.message_handler(commands=['Start','Greet'])
 def greet(message):
     bot.reply_to(message,"Hola!, como va todo?")
 
-bot.polling()
+#Mensaje ante cualquier comando no valido o texto
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.send_message(message.chat.id, 'Introduce un comando valido')
+
+
+
+bot.polling() #Buscando entrada de mensajes
